0291b71fd93f6c90a1b9bb0f0aa63b2345aa68ca
Rabel Obispo <rabelobispo@hotmail.com>
2017-12-21 23:17:26 -0400

    WebApi
    First webapi in core

---
+++ WebApi/README.md
@@ -0,0 +1,6 @@
+# Webapi
+
+This is a "Webapi of user" from scratch in ASP.Net Core MVC
+
+## Steps
+
---
+++ WebApi/UserWebApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using UserWebApi.Models;
+
+namespace UserWebApi.Controllers
+{
+    public class UserController : BaseController
+    {
+        public UserController(UserContext _userContext) : base(_userContext) { }
+
+        [HttpGet]
+        public IEnumerable<UserEntity> GetUserAll()
+        {
+            return
+                _userContext.UserEntities.ToArray();
+        }
+
+        [HttpGet("{userLogin}")]
+        public UserEntity GetUser(string userLogin)
+        {
+            return
+                _userContext.UserEntities.FirstOrDefault(u => u.UserLogin == userLogin);
+        }
+    }
+}
