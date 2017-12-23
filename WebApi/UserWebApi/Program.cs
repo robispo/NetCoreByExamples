0a15c83fae1787af2c251b327a76a3648d49b78a
Javis Perez <javisperez@gmail.com>
2017-12-23 00:03:46 -0400

    Port config and remove validating file existance from nginx

--- WebApi/UserWebApi/Program.cs
+++ WebApi/UserWebApi/Program.cs
@@ -20,6 +20,7 @@ namespace UserWebApi
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000")
                 .Build();
     }
 }
