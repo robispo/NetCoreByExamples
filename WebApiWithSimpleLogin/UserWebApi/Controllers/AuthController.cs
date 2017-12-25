563a2d5b612db8dc9adf3dbbd0cf46cb32c0f512
Rabel Obispo <rabelobispo@hotmail.com>
2017-12-25 11:39:41 -0400

    Changes

---
+++ WebApiWithSimpleLogin/UserWebApi/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserWebApi.Models;
+using UserWebApi.Services;
+
+namespace UserWebApi.Controllers
+{
+    [Authorize]
+    [Route("api/auth")]
+    public class AuthController : BaseController
+    {
+        public AuthController(UserContext userContext, IJwtService jwtService) : base(userContext, jwtService) { }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult LogIn([FromBody]AuthModel authModel)
+        {
+            if (authModel != null && _userContext.UserEntities.Any(u => u.UserLogin == authModel.UserName && u.Password == authModel.Password))
+            {
+                IEnumerable<Claim> claims;
+                claims = authModel.UserName == "robispo"
+                            ? new[] { new Claim(ClaimTypes.Name, authModel.UserName), new Claim("SuperTester", "true") }
+                            : new[] { new Claim(ClaimTypes.Name, authModel.UserName) };
+
+                _jwtService.GenerateToken(Request.HttpContext, claims);
+
+                return
+                    Ok();
+            }
+            else
+            {
+                return
+                    BadRequest(new { Message = "Invalid user or password." });
+            }
+        }
+
+        [HttpGet("Test")]
+        public IActionResult Test()
+        {
+            return
+                Ok(new { Message = "You are authorized!!!" });
+        }
+
+        [HttpGet("SuperTest")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Policy = "AdminTest")]
+        public IActionResult SuperTest()
+        {
+            return
+                Ok(new { Message = "You are SupperTester authorized!!!" });
+        }
+    }
+}
---
+++ WebApiWithSimpleLogin/UserWebApi/Startup.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using UserWebApi.Models;
+using UserWebApi.Services;
+
+namespace UserWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            JwtService jwtService;
+
+            services.AddSingleton<IConfiguration>(Configuration);
+
+            jwtService = new JwtService(Configuration);
+            services.Add(new ServiceDescriptor(typeof(IJwtService), jwtService));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => jwtService.RulesTokenValidation(options));
+
+            services.AddDbContext<UserContext>(opt => opt.UseInMemoryDatabase("WebapiTest"));
+
+            services.AddMvc()
+                .AddJsonOptions(opt =>
+                {
+                    var resolver = opt.SerializerSettings.ContractResolver;
+                    if (resolver != null)
+                    {
+                        var res = resolver as DefaultContractResolver;
+                        res.NamingStrategy = new SnakeCaseNamingStrategy();
+                    }
+                }); ;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
