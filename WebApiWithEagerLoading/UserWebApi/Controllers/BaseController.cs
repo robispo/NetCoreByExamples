de6a287e42b426902614bd1c316b3327a79b50f1
Rabel Obispo <rabelobispo@hotmail.com>
2017-12-25 22:33:29 -0400

    Working

---
+++ WebApiWithEagerLoading/UserWebApi/Controllers/BaseController.cs
@@ -0,0 +1,214 @@
+﻿using UserWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using UserWebApi.Services;
+using System.Collections.Generic;
+
+namespace UserWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class BaseController : Controller
+    {
+        protected readonly UserContext _userContext;
+        protected readonly IJwtService _jwtService;
+
+        public BaseController(UserContext userContext, IJwtService jwtService)
+        {
+            _userContext = userContext;
+            _jwtService = jwtService;
+            this.DefaultAdd();
+        }
+
+        private void DefaultAdd()
+        {
+            if (_userContext.UserEntities.Count() == 0)
+            {
+                #region Role
+                //_userContext.RoleEntities.Add(new RoleEntity
+                //{
+                //    Id = 1,
+                //    Role = "Adminstrator"
+                //});
+
+                //_userContext.RoleEntities.Add(new RoleEntity
+                //{
+                //    Id = 2,
+                //    Role = "User"
+                //});
+
+                //_userContext.RoleEntities.Add(new RoleEntity
+                //{
+                //    Id = 3,
+                //    Role = "Supervisor"
+                //});
+                #endregion
+
+                #region Permission
+                //_userContext.PermissionEntities.Add(new PermissionEntity
+                //{
+                //    Id = 1,
+                //    Permission = "CreateUser"
+                //});
+
+                //_userContext.PermissionEntities.Add(new PermissionEntity
+                //{
+                //    Id = 2,
+                //    Permission = "ReadUser"
+                //});
+
+                //_userContext.PermissionEntities.Add(new PermissionEntity
+                //{
+                //    Id = 3,
+                //    Permission = "UpdateUser"
+                //});
+                //_userContext.PermissionEntities.Add(new PermissionEntity
+                //{
+                //    Id = 4,
+                //    Permission = "DeleteUser"
+                //});
+                //_userContext.PermissionEntities.Add(new PermissionEntity
+                //{
+                //    Id = 5,
+                //    Permission = "ApproveUser"
+                //});
+                #endregion
+
+                #region RolePermission
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 1,
+                //    PermissionId = 1,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 1)
+                //});
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 1,
+                //    PermissionId = 2,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 2)
+                //});
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 1,
+                //    PermissionId = 3,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 3)
+                //});
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 1,
+                //    PermissionId = 4,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 4)
+                //});
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 1,
+                //    PermissionId = 5,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 5)
+                //});
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 2,
+                //    PermissionId = 2,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 2)
+                //});
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 3,
+                //    PermissionId = 2,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 2)
+                //});
+                //_userContext.RolePermissionEntities.Add(new RolePermissionEntity
+                //{
+                //    RoleId = 3,
+                //    PermissionId = 5,
+                //    Permission = _userContext.PermissionEntities.FirstOrDefault(p => p.Id == 5)
+                //});
+                #endregion
+
+                #region User
+                _userContext.UserEntities.Add(new UserEntity
+                {
+                    Id = 1,
+                    UserLogin = "robispo",
+                    Email = "rabelobispo@hotmail.com",
+                    Address = "Address",
+                    FirstName = "Rabel",
+                    LastName = "Obispo",
+                    NickName = "robispo",
+                    Password = "robispo",
+                    Roles = new RoleEntity[] { new RoleEntity { Id = 1, Role = "Adminstrator", Permissions = new PermissionEntity[] { new PermissionEntity { Id = 1, Permission = "CreateUser" } } } }
+                });
+
+                _userContext.UserEntities.Add(new UserEntity
+                {
+                    Id = 2,
+                    UserLogin = "jperez",
+                    Email = "jperez@hotmail.com",
+                    Address = "Address1",
+                    FirstName = "Javis",
+                    LastName = "Perez",
+                    NickName = "jperez",
+                    Password = "jperez",
+                    //Roles = new UserRoleEntity[] { new UserRoleEntity { UserId = 2, RoleId = 3 }, new UserRoleEntity { UserId = 2, RoleId = 2 } }
+                });
+
+                _userContext.UserEntities.Add(new UserEntity
+                {
+                    Id = 3,
+                    UserLogin = "jdeleon",
+                    Email = "jdeleon@hotmail.com",
+                    Address = "Address3",
+                    FirstName = "Jose",
+                    LastName = "De Leon",
+                    NickName = "jdeleon",
+                    Password = "jdeleon",
+                    //Roles = new UserRoleEntity[] { new UserRoleEntity { UserId = 3, RoleId = 2 } }
+                });
+                #endregion
+
+                #region UserOld
+                //_userContext.UserEntities.Add(new UserEntity
+                //{
+                //    Id = 1,
+                //    UserLogin = "robispo",
+                //    Email = "rabelobispo@hotmail.com",
+                //    Address = "Address",
+                //    FirstName = "Rabel",
+                //    LastName = "Obispo",
+                //    NickName = "robispo",
+                //    Password = "robispo",
+                //    Roles = new UserRoleEntity[] { new UserRoleEntity { UserId = 1, RoleId = 1 } }
+                //});
+
+                //_userContext.UserEntities.Add(new UserEntity
+                //{
+                //    Id = 2,
+                //    UserLogin = "jperez",
+                //    Email = "jperez@hotmail.com",
+                //    Address = "Address1",
+                //    FirstName = "Javis",
+                //    LastName = "Perez",
+                //    NickName = "jperez",
+                //    Password = "jperez",
+                //    Roles = new UserRoleEntity[] { new UserRoleEntity { UserId = 2, RoleId = 3 }, new UserRoleEntity { UserId = 2, RoleId = 2 } }
+                //});
+
+                //_userContext.UserEntities.Add(new UserEntity
+                //{
+                //    Id = 3,
+                //    UserLogin = "jdeleon",
+                //    Email = "jdeleon@hotmail.com",
+                //    Address = "Address3",
+                //    FirstName = "Jose",
+                //    LastName = "De Leon",
+                //    NickName = "jdeleon",
+                //    Password = "jdeleon",
+                //    Roles = new UserRoleEntity[] { new UserRoleEntity { UserId = 3, RoleId = 2 } }
+                //});
+                #endregion
+
+                _userContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ WebApiWithEagerLoading/UserWebApi/Models/UserModel.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserWebApi.Models
+{
+    public class UserEntity
+    {
+        public int Id { get; set; }
+        public string UserLogin { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<RoleEntity> Roles { get; set; }
+    }
+
+    //public class UserRoleEntity
+    //{
+    //    [Column(Order = 0), Key]
+    //    public int UserId { get; set; }
+    //    [Column(Order = 1), Key]
+    //    public int RoleId { get; set; }
+
+    //}
+
+    //public class RolePermissionEntity
+    //{
+    //    [Column(Order = 0), Key]
+    //    public int RoleId { get; set; }
+    //    [Column(Order = 1), Key]
+    //    public int PermissionId { get; set; }
+    //}
+
+    public class RoleEntity
+    {
+        public int Id { get; set; }
+        public string Role { get; set; }
+        public ICollection<PermissionEntity> Permissions { get; set; }
+    }
+
+    public class PermissionEntity
+    {
+        public int Id { get; set; }
+        public string Permission { get; set; }
+    }
+
+    public class JwtTokenEntity
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<RolePermissionEntity>().HasKey(t => new { t.RoleId, t.PermissionId });
+        //    modelBuilder.Entity<UserRoleEntity>().HasKey(t => new { t.UserId, t.RoleId });
+
+        //    base.OnModelCreating(modelBuilder);
+        //}
+
+        public DbSet<UserEntity> UserEntities { get; set; }
+        public DbSet<JwtTokenEntity> JwtTokenEntities { get; set; }
+        //public DbSet<UserRoleEntity> UserRoleEntities { get; set; }
+        //public DbSet<RolePermissionEntity> RolePermissionEntities { get; set; }
+        public DbSet<RoleEntity> RoleEntities { get; set; }
+        public DbSet<PermissionEntity> PermissionEntities { get; set; }
+
+    }
+}
