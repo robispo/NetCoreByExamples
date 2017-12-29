e198d6920a5fa76bb2ebab2c97490397e27eda68
Rabel Obispo <rabelobispo@hotmail.com>
2017-12-29 00:40:55 -0400

    Changes
    Dont work

---
+++ WebApiUnitTest/UserWebApi.UnitTest/UserWebApiUnitTest.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserWebApi.Controllers;
+using UserWebApi.Models;
+using UserWebApi.Services;
+
+namespace UserWebApi.UnitTest
+{
+    [TestClass]
+    public class UserWebApiUnitTest
+    {
+        UserController _userController;
+        DataBaseELContext _dataBaseELContext;
+        IJwtService _jwtService;
+
+        public UserWebApiUnitTest()
+        {
+            DbContextOptions<DataBaseELContext> dbOptions;
+            DbContextOptionsBuilder<DataBaseELContext> dboBuilder = new DbContextOptionsBuilder<DataBaseELContext>();
+            dboBuilder.UseInMemoryDatabase("WebapiTest");
+            dbOptions = dboBuilder.Options;
+
+            _dataBaseELContext = new DataBaseELContext(dbOptions);
+            _jwtService = new JwtService();
+
+            _userController = new UserController(_dataBaseELContext, _jwtService);
+        }
+
+        [TestMethod]
+        public void GetUserAll()
+        {
+            IActionResult actionResult;
+            ObjectResult objectResult;
+
+            actionResult = _userController.GetUserAll();
+
+            Assert.IsNotNull(actionResult);
+            objectResult = (ObjectResult)actionResult;
+
+            Assert.IsNotNull(objectResult.StatusCode);
+
+            Assert.AreEqual(objectResult.StatusCode.Value, 200);
+        }
+
+        [TestMethod]
+        public void GetUserWithData()
+        {
+            IActionResult actionResult;
+            ObjectResult objectResult;
+
+            actionResult = _userController.GetUser("robispo");
+
+            Assert.IsNotNull(actionResult);
+            objectResult = (ObjectResult)actionResult;
+
+            Assert.IsNotNull(objectResult.StatusCode);
+            Assert.IsNotNull(objectResult.Value);
+
+            Assert.AreEqual(objectResult.StatusCode.Value, (int)HttpStatusCode.OK);
+
+        }
+
+        [TestMethod]
+        public void GetUserWithNotData()
+        {
+            IActionResult actionResult;
+            ObjectResult objectResult;
+
+            actionResult = _userController.GetUser("");
+
+            Assert.IsNotNull(actionResult);
+            objectResult = (ObjectResult)actionResult;
+
+            Assert.IsNotNull(objectResult.StatusCode);
+
+            Assert.AreEqual(objectResult.StatusCode.Value, (int)HttpStatusCode.BadRequest);
+        }
+
+        [TestMethod]
+        public void GetUserWithEagerLoading()
+        {
+
+            //var builder = new HttpRequestBuilder()
+            //                    .AddMethod(HttpMethod.Get)
+            //                    .AddRequestUri(requestUri);
+
+            //return await builder.SendAsync();
+
+
+            
+
+            //tResponse.Start();
+            //response = tResponse.Result;
+
+            Assert.AreEqual(Response.StatusCode, HttpStatusCode.OK);
+
+        }
+
+        public static string TestWebApiUrl()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8610/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (HttpResponseMessage response = await client.GetAsync("api/users/robispo?with=Roles"))
+                {
+
+                }
+
+            }
+
+
+            return
+                string.Empty;
+        }
+    }
+}
